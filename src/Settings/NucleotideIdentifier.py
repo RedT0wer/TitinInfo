762f6b388bb5beb06ad1e06f6efcc76c03500d12
RedT0wer <sawitar78@mail.ru>
2025-05-28 09:10:01 +0400

    create environment for nucleotide if

--- src/Settings/NucleotideIdentifier.py
+++ src/Settings/NucleotideIdentifier.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, create_model
 
-class NucleotideIdentifier(BaseSettings):
+class InitialModel(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="nucleotide_identifier.env",
+        env_file="Settings/nucleotide_identifier.env",
         env_file_encoding="utf-8",
         extra="allow"
     )
+
+initialModel = InitialModel()
+env_vars = initialModel.model_dump()
+fields = {k: (str, Field(default=v)) for k, v in env_vars.items()}
+NucleotideIdentifier = create_model("NucleotideIdentifier", **fields, __base__=BaseSettings)
 nucleotideIdentifier = NucleotideIdentifier()
-print(nucleotideIdentifier.model_dump())
